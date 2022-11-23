1d958ee82a0a64917617a670aa0c3cfe1bccb096
aCute5 <costantini95dario@gmail.com>
2022-11-23 10:59:55 +0100

    v-vor added

--- js/script.js
+++ js/script.js
@@ -5,8 +5,7 @@ new Vue({
 			arrMails: [],
 	},
 	methods: {
-		getNumbers() {
-			this.arrNumbers = [];
+		getMails() {
 			for (let i = 0; i < 10; i++) {
 				axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 				.then(axiosResponse => {
